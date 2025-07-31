da53497d3857d79d48896130f42e5b86facd78cc
Takamaru <takamaru@example.com>
2025-07-31 11:17:03 +0900

    "GoogleログインのリダイレクトURLを修正"

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -30,7 +30,7 @@ export default function LoginForm() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${location.origin}/auth/callback`,
+        redirectTo: `${location.origin}/auth/callback/`,
       },
     })
     if (error) {
